0866914177c1db4e2835a9df62faab45416909c3
raulperez0481 <raulperez0481@gmail.com>
2023-06-29 13:29:51 +0200

    Migrada la css a scss nesting

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 const colorOption1 = document.querySelector('.card__color--option1');
 const colorOption2 = document.querySelector('.card__color--option2');
-const image = document.querySelector('.card__img');
 const card = document.querySelector('.card');
+const image = document.querySelector('.card__img');
 
 colorOption1.addEventListener('click', () => {
-  image.src = '/images/shoe1.png';
-  card.style.backgroundColor = 'var(--bg-color-option1)';
-})
+  image.src = '../images/shoe1.png';
+  card.classList.remove('color-option2');
+  card.classList.add('color-option1');
+});
 
 colorOption2.addEventListener('click', () => {
-  image.src = '/images/shoe2.png';
-  card.style.backgroundColor = 'var(--bg-color-option2)';
-})
+  image.src = '../images/shoe2.png';
+  card.classList.remove('color-option1');
+  card.classList.add('color-option2');
+});
