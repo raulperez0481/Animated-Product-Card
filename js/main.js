6fd03a232db57a14ba16edadd51e3bd7dd1f4ce6
raulperez0481 <raulperez0481@gmail.com>
2023-06-30 09:41:25 +0200

    solucionado ruta imagen desde js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ const card = document.querySelector('.card');
 const image = document.querySelector('.card__img');
 
 colorOption1.addEventListener('click', () => {
-  image.src = '../images/shoe1.png';
+  image.src = 'images/shoe1.png';
   card.classList.remove('color-option2');
   card.classList.add('color-option1');
 });
 
 colorOption2.addEventListener('click', () => {
-  image.src = '../images/shoe2.png';
+  image.src = 'images/shoe2.png';
   card.classList.remove('color-option1');
   card.classList.add('color-option2');
 });
